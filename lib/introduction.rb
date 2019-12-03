4e9994f14a5058c65ebc67f56fa809f740432676
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-03 02:13:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,11 +9,11 @@ def #introduction(name)
   puts "Hi my namne is #{name}"
 end
 
-def introduction_with_language(name , language)
+def #introduction_with_language(name , language)
   puts "Hi my name is #{name} and i am learning to program in #{language}"
 end
 
-def introduction_with_language_optional(name , language=ruby)
+def #introduction_with_language_optional(name , language=ruby)
   puts "Hi my name is #{name} and i am learning to program in #{language}"
 end
   
